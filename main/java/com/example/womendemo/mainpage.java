a79c8e082b8db4c8d9dee35190863a91bca6497a
Pankti Sugandhi <panktisugandhi143@gmail.com>
2019-04-12 22:00:53 +0530

    siren btn/ setting btn

--- main/java/com/example/womendemo/mainpage.java
+++ main/java/com/example/womendemo/mainpage.java
@@ -1,7 +1,9 @@
 package com.example.womendemo;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,9 +16,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class mainpage extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,28 @@ public class mainpage extends AppCompatActivity
         setContentView(R.layout.activity_mainpage);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Button redbtn = (Button) findViewById(R.id.redbtn);
+        Button greenbtn = (Button) findViewById(R.id.greenbtn);
+        Button orangebtn = (Button) findViewById(R.id.orengebtn);
+
+        redbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mainpage.this, "Pressed Red Panic Alert", Toast.LENGTH_SHORT).show();
+            }
+        });
+        greenbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mainpage.this, "Pressed Green Status Update", Toast.LENGTH_SHORT).show();
+            }
+        });
+        orangebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mainpage.this, "Pressed Orange Cautious", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -60,39 +87,69 @@ public class mainpage extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.siren) {
+
+        }
+        else if (id== R.id.setting){
+
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
+
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.personal) {
-            // Handle the camera action
-        } else if (id == R.id.relative) {
+        switch (id) {
+            case R.id.personal:
+                Toast.makeText(this, "Personal details", Toast.LENGTH_SHORT).show();
+//                Intent intent1 = new Intent(mainpage.this,personal_detail.class);
+//                startActivity(intent1);
+                break;
 
-        } else if (id == R.id.setting) {
-//            Intent i = new Intent(this,gensetting.class);
-//            startActivity(i);
-        } else if (id == R.id.help) {
+            case R.id.relative:
+                Toast.makeText(this, "Relative details", Toast.LENGTH_SHORT).show();
+                break;
 
-        } else if (id == R.id.feedback) {
+            case  R.id.setting:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+//                Intent i = new Intent(mainpage.this, gensetting.class);
+//                startActivity(i);
+                break;
+
+            case R.id.help:
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/panktisugandhi/Women-Safety/blob/master/help.txt"));
+                startActivity(intent);
+                break;
+
+            case R.id.feedback:
+                Intent Email = new Intent(Intent.ACTION_SEND);
+                Email.setType("text/email");
+                Email.putExtra(Intent.EXTRA_EMAIL, new String[]{"panktisugandhi143@gmail.com"});
+                Email.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
+                Email.putExtra(Intent.EXTRA_TEXT, "Dear ...," + "");
+                startActivity(Intent.createChooser(Email, "Send Feedback:"));
+                break;
+
+            case  R.id.nav_share:
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+                break;
+            case R.id.rate:
+                Intent in = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/panktisugandhi"));
+                startActivity(in);
+                break;
 
         }
-        else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
---
+++ main/java/com/example/womendemo/personal_detail.java
@@ -0,0 +1,16 @@
+package com.example.womendemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class personal_detail extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_detail);
+
+
+
+    }
+}
